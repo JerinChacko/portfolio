e3337fcaa77cb276e9f8cc3dfd2ff661c65c195f
Jerin <jerinchacko029@gmail.com>
2022-02-02 00:53:48 -0800

    added home section

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,6 +2,7 @@
 const navMenu = document.getElementById('nav-menu');
       navToggle = document.getElementById('nav-toggle');
       navClose = document.getElementById('nav-close');
+    
 
 /*=== MENU SHOW ===============*/
 /*== VALIDATE IF CONSTANT EXISTS=====*/
@@ -20,11 +21,11 @@ if(navClose){
 }
 
 /*====== REMOVE MOBILE MENU =======*/
-const navLink = document.querySelectorAll('.nav__link');
+const  navLink = document.querySelectorAll(".nav_link")
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu');
+    const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
+    navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
